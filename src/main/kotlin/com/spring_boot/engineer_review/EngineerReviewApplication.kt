650a30833ba13a8c8bdbc6bae87c3aad2092cd8f
Takuya Natsume <xxtyzs@gmail.com>
2021-01-18 18:49:19 +0900

    first commit

---
+++ src/main/kotlin/com/spring_boot/engineer_review/EngineerReviewApplication.kt
@@ -0,0 +1,11 @@
+package com.spring_boot.engineer_review
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EngineerReviewApplication
+
+fun main(args: Array<String>) {
+	runApplication<EngineerReviewApplication>(*args)
+}
