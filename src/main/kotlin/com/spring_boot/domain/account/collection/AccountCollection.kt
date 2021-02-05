e435c98a4f0a3aebe88f0f8ed0eb5ce6ace2feac
Takuya Natsume <xxtyzs@gmail.com>
2021-02-05 11:30:19 +0900

    #13 implement ReviewCollection

--- src/main/kotlin/com/spring_boot/domain/account/collection/AccountCollection.kt
+++ src/main/kotlin/com/spring_boot/domain/account/collection/AccountCollection.kt
@@ -4,7 +4,7 @@ import com.spring_boot.base.collection.AbstractEntityCollection
 import com.spring_boot.domain.account.Account
 
 /**
- * @return [AccountCollection] converted from [List<Book>]
+ * @return [AccountCollection] converted from [List<Account>]
  */
 fun List<Account>.toCollection(): AccountCollection {
     return AccountCollection(this)
